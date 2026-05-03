532c60cde65bb903126ebbe1ba4bce729afb85b0
Ikkch-Matthew <matuha570@gmail.com>
2026-05-03 23:28:14 +0700

    remade comments

--- src/syn/uart2/uart.sv
+++ src/syn/uart2/uart.sv
@@ -239,7 +239,8 @@ always_ff @(posedge clk) begin
         overrun     <= 1'b0;
         frame_error <= 1'b0;
     end
-    if (rx_rden) rx_complete <= 1'b0;               // Catch rx_rden
+    if (rx_rden)
+        rx_complete <= 1'b0;
     case (rx_state)
     RX_IDLE: begin
         rx_stat     <= RX_STATE_HOLD;
@@ -276,8 +277,10 @@ always_ff @(posedge clk) begin
         rx_stat     <= RX_STATE_HOLD;
         rx_timer_en <= 1;
         if (rx_timer == BIT_PERIOD - 1) begin
-            if (!rxc_shift[2]) frame_error <= 1'b1;   // Checking STOP bit
-            if ( rx_complete ) overrun     <= 1'b1;   // Overrun
+            if (!rxc_shift[2]) 
+                frame_error <= 1'b1;
+            if ( rx_complete ) 
+                overrun     <= 1'b1;
             rx_data     <= rx_shift;
             rx_complete <= 1'b1;
             rx_stat     <= RX_STATE_NEXT;

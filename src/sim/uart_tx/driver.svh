586fc2c5e2b00c5cfd9b3a76a10baece07729516
Matthew <matuha570@gmail.com>
2026-05-12 11:12:55 +0700

    remade receiving tx_data to uart (driver)

--- src/sim/uart_tx/driver.svh
+++ src/sim/uart_tx/driver.svh
@@ -37,16 +37,8 @@ class Driver;
     
             @(posedge uif.clk) uif.tx_wren = 1;
             @(posedge uif.clk) uif.tx_wren = 0;
-            
-            #100ns;
-            
-            if(uif.tx_empty)
-                uif.tx_data = tx_tr_drv.data;
-                
-            @(posedge uif.clk) uif.tx_wren = 1;
-            @(posedge uif.clk) uif.tx_wren = 0;
-              
-            wait(uif.tx_complete);
+
+            #20ns;
             
             num_trn_tx++;
         

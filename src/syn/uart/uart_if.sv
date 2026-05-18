375fde918c86599b938ef047a56c468cc86a3c00
Matthew <matuha570@gmail.com>
2026-05-18 15:28:27 +0700

    remade interface

--- src/syn/uart/uart_if.sv
+++ src/syn/uart/uart_if.sv
@@ -13,6 +13,7 @@ interface uart_if;
     logic            init_en;
 
     logic [WORD-1:0] rx_data;
+    logic [WORD-1:0] rx_buffer;
     logic            rx_rden;
     logic            rx_complete;
     logic            frame_error;
@@ -21,6 +22,7 @@ interface uart_if;
     logic            rx_done;
 
     logic [WORD-1:0] tx_data;
+    logic [WORD-1:0] tx_buffer;
     logic            tx_wren;
     logic            tx_empty;
     logic            tx_complete;
@@ -37,15 +39,17 @@ interface uart_if;
         input  rx_rden,
         input  rst_err,
         input  rx_done,
+        input  rx_buffer,
         output rx_data,
         output rx_complete,
         output frame_error,
         output overrun,
         
-        input  txc,
         input  tx_wren,
         input  tx_done,
         input  tx_empty_clr,
+        output txc,
+        output tx_buffer,
         output tx_data,
         output tx_empty,
         output tx_complete
@@ -55,6 +59,7 @@ interface uart_if;
     (
         output clk,
         output baud_pulse,
+        
         output rxc,
         output rx_rden,
         output rst_err,
@@ -63,8 +68,8 @@ interface uart_if;
         input  frame_error,
         input  overrun,
         
-        output txc,
         output tx_wren,
+        input  txc,
         input  tx_data,
         input  tx_empty,
         input  tx_complete

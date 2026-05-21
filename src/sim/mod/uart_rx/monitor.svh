87459c37f52c6a21e87b64f60053c7098747a27b
Matthew <matuha570@gmail.com>
2026-05-21 18:46:05 +0700

    relocate coverage groups from mnt to scb

--- src/sim/mod/uart_rx/monitor.svh
+++ src/sim/mod/uart_rx/monitor.svh
@@ -13,50 +13,11 @@ class Monitor;
 
     mailbox #(mnt_rcvd_t) mnt2scb_rx;
     
-    covergroup rx_data_cg @(posedge vif.clk);
-        rx_data : coverpoint vif.rx_data
-        {
-            bins dat_0   = {    0    };
-            bins dat_63  = {[  1:63 ]};
-            bins dat_127 = {[ 64:127]};
-            bins dat_254 = {[128:254]};
-            bins dat_255 = {   255   };
-        }
-        rx_err_fr : coverpoint vif.frame_error
-        {
-            bins err_0 = {0};
-            bins err_1 = {1};
-        }
-        rx_err_run : coverpoint vif.overrun
-        {
-            bins err_0 = {0};
-            bins err_1 = {1};
-        }
-
-        option.per_instance = 1;
-
-    endgroup
-
-    covergroup rx_del_cg;
-        rx_rden_delay : coverpoint rx_tr_mnt.rden_delay
-        {
-            bins del_0  = {      0      };
-            bins del_10 = {[    1:10000]};
-            bins del_20 = {[10001:20000]};
-            bins del_30 = {[20000:30000]};
-        }
-
-        option.per_instance = 1;
-
-    endgroup
-    
     function new(mailbox #(mnt_rcvd_t) mnt2scb_rx ,
                  virtual               uart_if vif);
     
         this.mnt2scb_rx = mnt2scb_rx;
         this.vif        = vif;
-        rx_data_cg      = new();
-        rx_del_cg       = new();
     
     endfunction 
     
@@ -75,7 +36,6 @@ class Monitor;
                         mnt_data.frame_error = vif.frame_error;
                         mnt_data.overrun     = vif.overrun;
                         mnt2scb_rx.put(mnt_data);
-                        rx_data_cg.sample();
 
                         if(vif.overrun | vif.frame_error) begin
                             reset_err();
@@ -114,7 +74,6 @@ class Monitor;
             @(posedge vif.clk) vif.rx_rden = 1;
             @(posedge vif.clk) vif.rx_rden = 0;
 
-            rx_del_cg.sample();
 
         end
 
--- src/sim/mod/uart_rx/scoreboard.svh
+++ src/sim/mod/uart_rx/scoreboard.svh
@@ -18,11 +18,54 @@ class Scoreboard;
     
     virtual uart_if uif;
     
+    covergroup rx_data_cg;
+        rx_data : coverpoint mnt_data.data
+        {
+            bins dat_0   = {    0    };
+            bins dat_63  = {[  1:63 ]};
+            bins dat_127 = {[ 64:127]};
+            bins dat_254 = {[128:254]};
+            bins dat_255 = {   255   };
+        }
+        rx_err_fr : coverpoint mnt_data.frame_error
+        {
+            bins err_0 = {0};
+            bins err_1 = {1};
+        }
+        rx_err_ov : coverpoint mnt_data.overrun
+        {
+            bins err_0 = {0};
+            bins err_1 = {1};
+        }
+
+    endgroup
+
+    covergroup rx_del_cg;
+        rx_rden_delay : coverpoint rx_tr_scb.rden_delay
+        {
+            bins del_0  = {      0      };
+            bins del_10 = {[    1:10000]};
+            bins del_20 = {[10001:20000]};
+            bins del_30 = {[20000:30000]};
+        }
+        
+        rx_send_delay : coverpoint rx_tr_scb.send_delay
+        {
+            bins del_0  = {      0      };
+            bins del_10 = {[    1:10000]};
+            bins del_20 = {[10001:20000]};
+            bins del_30 = {[20000:30000]};
+        }
+
+    endgroup
+    
                  mailbox #(mnt_rcvd_t) mnt2scb_rx ,
                  semaphore             sem_scb2drv);
     
         this.mnt2scb_rx  = mnt2scb_rx;
         this.sem_scb2drv = sem_scb2drv;
+        rx_data_cg       = new();
+        rx_del_cg        = new();
     
     endfunction
     
@@ -67,6 +110,8 @@ class Scoreboard;
 
             end
             num_trn_rx++;
+            rx_data_cg.sample();
+            rx_del_cg.sample();
 
         end
 

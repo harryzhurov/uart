fc25b8a545bebce8f63be361545173a1d1ccb021
Matthew <matuha570@gmail.com>
2026-05-22 17:12:39 +0700

    remade tx scb with mailbox

--- src/sim/mod/uart_tx/scoreboard.svh
+++ src/sim/mod/uart_tx/scoreboard.svh
@@ -4,43 +4,51 @@
 //
 class Scoreboard;
 
-    int err        = 0;
-    int num_trn_tx = 0;
+    int err          = 0;
+    int num_trn_tx   = 0;
+    int percent      =  0;
+    int last_percent = -1;
     
-    data_t tx_data_shift;
-    
-    tx_trn_t tx_tr_scb;
+    tx_pak_t tx_drv_scb;
+    tx_pak_t tx_mnt_scb;
 
-    mailbox #(tx_trn_t) gen2scb_tx;
-    mailbox #( data_t ) mnt2scb_tx;
+    mailbox #(tx_pak_t) drv2scb_tx;
+    mailbox #(tx_pak_t) mnt2scb_tx;
     
-    semaphore sem_scb2drv;
+    function new(mailbox #(tx_pak_t) drv2scb_tx,
+                 mailbox #(tx_pak_t) mnt2scb_tx);
     
-    function new(mailbox #(tx_trn_t) gen2scb_tx ,
-                 mailbox #( data_t ) mnt2scb_tx ,
-                 semaphore           sem_scb2drv);
-    
-        this.gen2scb_tx  = gen2scb_tx ;
+        this.drv2scb_tx  = drv2scb_tx ;
         this.mnt2scb_tx  = mnt2scb_tx ;
-        this.sem_scb2drv = sem_scb2drv;
     
+    endfunction
+    
+    function void meas_percent;
+        percent = (this.num_trn_tx*100) / (trn_cfg_pkg::num_trn_tx);
+
+        if (percent != last_percent && (percent % 10 == 0 || percent == 100)) begin
+            $display("INFO: Scoreboard completed %0d%% (%0d/%0d)", percent, this.num_trn_tx, trn_cfg_pkg::num_trn_tx);
+            last_percent = percent;
+        end
     endfunction
     
     task automatic check_tx();
     
         forever begin
         
-            gen2scb_tx.get(tx_tr_scb);
-            mnt2scb_tx.get(tx_data_shift);
-            if(tx_tr_scb.data !== tx_data_shift) begin
+            drv2scb_tx.get(tx_drv_scb);
+            mnt2scb_tx.get(tx_mnt_scb);
+            if(tx_drv_scb.data !== tx_mnt_scb.data) begin
             
-                $display("INFO (ERROR) (tx) : bad frame = %d, time = [%t]",num_trn_tx, $realtime);
-                $display("      Sent data = %h, Received = %h",tx_tr_scb.data,tx_data_shift);
+                $display("INFO (ERROR) (tx) : bad frame, id : drv = %d, mnt = %d",tx_drv_scb.id, tx_mnt_scb.id);
+                $display("      Sent data = %h, Received = %h",tx_drv_scb.data, tx_mnt_scb.data);
                 err++;
                 
             end
             
             num_trn_tx++;
+            
+            meas_percent;
         
         end
         

ef790b9677660cccf32ff2eb23484b8df35cbedb
Matthew <matuha570@gmail.com>
2026-05-26 14:55:44 +0700

    change parameters to localparams

--- src/syn/uart/params_pkg.sv
+++ src/syn/uart/params_pkg.sv
@@ -4,12 +4,12 @@ package params_pkg;
 //
 //      Parameters
 //
-parameter int WORD            = 8   ;
-parameter int CLK_FREQ        = 100_000_000;
-parameter int BAUD_RATE       = 115200;
-parameter int BIT_PERIOD      = CLK_FREQ / BAUD_RATE;
-parameter int HALF_PERIOD     = BIT_PERIOD / 2;
-parameter int CLK_CYCLE       = 1_000_000_000/CLK_FREQ;
-parameter int UART_CYCLE      = BIT_PERIOD*CLK_CYCLE;
+localparam int WORD            = 8   ;
+localparam int CLK_FREQ        = 100_000_000;
+localparam int BAUD_RATE       = 115200;
+localparam int BIT_PERIOD      = CLK_FREQ / BAUD_RATE;
+localparam int HALF_PERIOD     = BIT_PERIOD / 2;
+localparam int CLK_CYCLE       = 1_000_000_000/CLK_FREQ;
+localparam int UART_CYCLE      = BIT_PERIOD*CLK_CYCLE;
 //===================================================================================
 endpackage : params_pkg

b19ebeca04c6cc6fcaac0d72772691cae95ab10f
Matthew <matuha570@gmail.com>
2026-06-22 17:58:50 +0700

    add virtual clk interface for correct verification

--- src/sim/intg/top_tb.sv
+++ src/sim/intg/top_tb.sv
@@ -12,91 +12,48 @@
 
 `include "uvm_macros.svh"
 `include "test.svh"
-//-------------------------------------------------------------------------------
 
-interface out_if;
-
-logic   clk;
-logic   frame_error;
-logic   overrun;
-uint8_t rx_data;
-logic   rx_complete;
-logic   tx_empty;
-logic   tx_complete;
-logic   txc;
-
+interface clk_if;
+    logic clk;
+    logic baud_pulse;
 endinterface
-
-interface inp_if;
-
-logic   clk;
-logic   rxc;
-logic   rst_err;
-logic   rx_rden;
-logic   tx_wren;
-uint8_t tx_data;
-logic   baud_pulse;
-
-endinterface
-
 //-------------------------------------------------------------------------------
-
 module automatic top_tb;
 
 import uvm_pkg::*;
-
 //-------------------------------------------------------------------------------
-
-logic  clk = 0;
-logic  baud_pulse = 0;
-inp_if inp();
-out_if out();
-
+clk_if  cv();
+uart_if pld();
 //-------------------------------------------------------------------------------
 
 initial begin
+    cv.clk        = 0;
+    cv.baud_pulse = 0;
     fork
         begin
-            forever #(CLK_CYCLE/2) clk = ~clk;
+            forever #(CLK_CYCLE/2) cv.clk = ~cv.clk;
         end
         begin
             #($urandom_range(0,UART_CYCLE));
             forever begin
-                #(UART_CYCLE - CLK_CYCLE) baud_pulse = 1;
-                #(CLK_CYCLE)              baud_pulse = 0;
+                #(UART_CYCLE - CLK_CYCLE) cv.baud_pulse = 1;
+                #(CLK_CYCLE)              cv.baud_pulse = 0;
             end
         end
     join
 end
 
-assign inp.clk        = clk;
-assign out.clk        = clk;
-assign inp.baud_pulse = baud_pulse;
-
 initial begin
-    uvm_config_db #(virtual inp_if )::set(null, "*", "inp", inp);
-    uvm_config_db #(virtual out_if )::set(null, "*", "out", out);
-
+    uvm_config_db #(virtual uart_if )::set(null, "*", "pld", pld);
+    uvm_config_db #(virtual clk_if )::set(null, "*", "cv", cv);
     run_test("UartTest");
 end
 
 //-------------------------------------------------------------------------------
-top top_inst
+uart dut
 (
-.clk            ( inp.clk         ),
-.rxc            ( inp.rxc         ),
-.rx_rden        ( inp.rx_rden     ),
-.rst_err        ( inp.rst_err     ),
-.rx_data        ( out.rx_data     ),
-.rx_complete    ( out.rx_complete ),
-.frame_error    ( out.frame_error ),
-.overrun        ( out.overrun     ),
-.baud_pulse     ( inp.baud_pulse  ),
-.txc            ( out.txc         ),
-.tx_empty       ( out.tx_empty    ),
-.tx_data        ( inp.tx_data     ),
-.tx_complete    ( out.tx_complete ),
-.tx_wren        ( inp.tx_wren     )
+.clk   ( cv.clk ),
+.pld   ( pld    )
 );
 //==================================================
 endmodule : top_tb
